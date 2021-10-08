e8fdf517b97f729605b82732ad745a8ed7c96a49
Alona Melnykova <alonamelnykova@gmail.com>
2021-10-08 14:02:21 +0300

    catch error fixed

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,12 +2,15 @@
 const urlRoute = 'https://restcountries.com/v2/name/';
 const filterName = '?fields=name,capital,population,flags,languages';
 export const fetchCountries = countryName =>
-  new Promise((res, rej) => {
-    fetch(urlRoute + countryName + filterName)
-      .then(response => {
-        if (response.status >= 200 && response.status < 300) return response.json();
-
-        rej('error request');
-      })
-      .then(data => res(data));
-  });
+  // new Promise((res, rej) => {
+  {
+    return fetch(urlRoute + countryName + filterName).then(response => {
+      if (response.status >= 200 && response.status < 300) return response.json();
+      // if (response.status === 404) {
+      //   throw new Error('404 error');
+      // }
+      // rej('error request');
+    });
+    // .then(data => res(data));
+  };
+// });
--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import Notiflix from 'notiflix';
 // ==========================================================
 
 const DEBOUNCE_DELAY = 300;
-console.log(fetchCountries('peru'));
-
 export const isJson = str => {
   try {
     JSON.parse(str);
@@ -20,45 +18,53 @@ const countryList = document.querySelector('.country-list');
 const countryItem = document.querySelector('.country-info');
 const inputNode = document.querySelector('input');
 const inputData = () => {
-  fetchCountries(inputNode.value.trim()).then(array => {
-    console.log(array);
-    if (array.length > 10) {
-      return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-    }
-    // if (!inputNode.value.includes(array[0].name)) {
-    //   return Notiflix.Notify.failure('Oops, there is no country with that name');
-    // }
+  fetchCountries(inputNode.value.trim())
+    .then(array => {
+      //   console.log(array);
+      if (array.length > 10) {
+        return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+      }
+      // if (!inputNode.value.includes(array[0].name)) {
+      //   return Notiflix.Notify.failure('Oops, there is no country with that name');
+      // }
 
-    if (array.length > 1 && array.length <= 10) {
-      countryItem.innerHTML = '';
-      const markup = array
-        .map(
-          item => `<li>
-                <img src="${item.flags.svg}"width="30" height="30" alt="${item.name}" >
+      if (array.length > 1 && array.length <= 10) {
+        countryItem.innerHTML = '';
+        const markup = array
+          .map(
+            item => `<li>
+                <img src="${item.flags.svg}"width="50" height="50" alt="${item.name}" >
                       ${item.name}</li>
                `,
-        )
-        .join('');
-      countryList.innerHTML = markup;
-    }
-    if (array.length === 1) {
-      countryList.innerHTML = '';
-      const markup = array
-        .map(
-          item => ` 
-                <ul>
-                <img src="${item.flags.svg}"width="30" height="30" alt="${item.name}" >
+          )
+          .join('');
+        countryList.innerHTML = markup;
+      }
+      if (array.length === 1) {
+        countryList.innerHTML = '';
+        const markup = array
+          .map(
+            item => ` 
+                <ul class= "counriesLimited">
+                <img src="${item.flags.svg}"width="50" height="50" alt="${item.name}" >
                     <li> имя страны: ${item.name}</li>
                     <li> столица: ${item.capital}</li>
                     <li> население: ${item.population}</li>
                     <li> языки: ${item.languages[0].name}</li>
                 </ul>
           `,
-        )
-        .join('');
-      countryItem.innerHTML = markup;
-    }
-  });
+          )
+          .join('');
+        countryItem.innerHTML = markup;
+      }
+      if (array.status === 404) {
+        return Notiflix.Notify.failure('Oops, there is no country with that name');
+      }
+    })
+    .catch(error => {
+      //   console.log('falseeee');
+      return Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
 };
 
 inputNode.addEventListener('input', debounce(inputData, DEBOUNCE_DELAY));
